d54363f5d77419486869751a7e7fd64450162636
LDBoshoff <luhahn.boshoff@gmail.com>
2023-10-05 22:25:12 +0200

    initialize product data

---
+++ backend/src/main/java/com/ldb/backend/model/Product.java
@@ -0,0 +1,80 @@
+package com.ldb.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity  // Annotate the class as an entity for JPA (Java Persistence API)
+@Table(name = "products")  // Specify the name of the table in the database
+public class Product {
+    
+    @Id  // Mark the 'id' attribute as the primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Use an auto-generated value for 'id' using identity strategy
+    private Long id;
+    
+    @Column(nullable = false)  // Define 'name' attribute for product name, not nullable
+    private String name;
+    
+    @Column(nullable = false)  // Define 'description' attribute for product description, not nullable
+    private String description;
+    
+    @Column(nullable = false)  // Define 'price' attribute for product price, not nullable
+    private double price;
+    
+    @Column(nullable = false)  // Define 'stockQuantity' attribute for product stock quantity, not nullable
+    private int stockQuantity;
+
+    protected Product() {
+        // Default constructor
+    }
+
+    public Product(String name, String description, double price, int stockQuantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+-- Products
+INSERT INTO products (name, description, price, stockQuantity) VALUES ('Smartphone', 'High-quality smartphone with advanced features.', 599.99, 100);
+INSERT INTO products (name, description, price, stockQuantity) VALUES ('Laptop', 'Powerful laptop with a fast processor and ample storage.', 999.99, 50);
+INSERT INTO products (name, description, price, stockQuantity) VALUES ('Wireless Headphones', 'Premium wireless headphones with noise-cancellation technology.', 199.99, 75);
+INSERT INTO products (name, description, price, stockQuantity) VALUES ('Smartwatch', 'Stylish smartwatch with health tracking and notification features.', 149.99, 60);
+INSERT INTO products (name, description, price, stockQuantity) VALUES ('Coffee Maker', 'High-quality coffee maker for brewing your favorite beverages.', 49.99, 120);
