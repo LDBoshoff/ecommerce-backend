bceff4e574841595ddd7b38a3800c53b8d5b3dda
LDBoshoff <luhahn.boshoff@gmail.com>
2023-10-06 00:02:30 +0200

    Added ProductService

--- backend/src/main/java/com/ldb/backend/repository/ProductRepository.java
+++ backend/src/main/java/com/ldb/backend/repository/ProductRepository.java
@@ -6,4 +6,33 @@ import com.ldb.backend.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     
+    // Available methods provided by JpaRepository:
+
+    // Save a product entity to the database
+    // Product save(Product product);
+
+    // Retrieve a product by its primary key (ID)
+    // Returns an Optional<Product> to handle cases where the product may not exist
+    // Optional<Product> findById(Long id);
+
+    // Retrieve all products in the database
+    // List<Product> findAll();
+
+    // Save a collection of product entities to the database
+    // List<Product> saveAll(Iterable<Product> products);
+
+    // Delete a product entity from the database
+    // void delete(Product product);
+
+    // Delete all product entities from the database
+    // void deleteAll();
+
+    // Delete product entities by their primary keys (IDs)
+    // void deleteAllByIdIn(Iterable<Long> ids);
+
+    // Check if a product entity exists in the database by its primary key (ID)
+    // boolean existsById(Long id);
+
+    // Count the number of product entities in the database
+    // long count();
 }
---
+++ backend/src/test/java/com/ldb/backend/ProductRepositoryTests.java
@@ -0,0 +1,15 @@
+package com.ldb.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ldb.backend.repository.ProductRepository;
+
+// @RunWith(SpringRunner.class)
+// @DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired 
+    private ProductRepository productRepository;
+
+
+}
